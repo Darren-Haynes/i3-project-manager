1ca1094ecc9673bff411afb1a52c52b6cae5a558
Darren-Haynes <darrenhaynes@zoho.com>
2017-07-10 07:15:20 -0700

    added globstar to scripts for correct opening of filetypes in vim. Also added parameter 'yes' to opening css and js files in vim, this sources different color themes

--- projects/pyscripting.sh
+++ projects/pyscripting.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-i3-msg "workspace Dotfiles; append_layout /home/darren/i3-project-manager/templates/pyscripting.json"
 
-cp ~/.Xresources ~/.which_color/xresources_vim
-(urxvt -e vim ~/Py_Scripts/* &)
-sleep 0.1
+shopt -s globstar
+
+i3-msg "workspace Project; append_layout /home/darren/i3-project-manager/templates/pyscripting.json"
+
 python ~/Py_Scripts/xr_random_colors.py
-cp ~/.Xresources ~/.which_color/xresources_ipython
+(urxvt -e vim ~/Py_Scripts/**/* &)
+sleep 0.1
+
+python ~/Py_Scripts/xr_random_colors.py
 (urxvt -e ipython &)
 sleep 0.01
+
 python ~/Py_Scripts/xr_random_colors.py
-cp ~/.Xresources ~/.which_color/xresources_ipython2
 (urxvt -e ipython2 &)
 sleep 0.01
+
 python ~/Py_Scripts/xr_random_colors.py
-cp ~/.Xresources ~/.which_color/xresources_urxvt
 (urxvt &)
 
