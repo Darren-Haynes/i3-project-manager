2d8b57c8bf4066645b9b39c0e0d1bff63537629c
Darren-Haynes <darrenhaynes@zoho.com>
2017-07-11 09:26:56 -0700

    added function to test if project already exists. Added more comments also

--- create_project.py
+++ create_project.py
@@ -8,15 +8,11 @@
    created and this script does the rest.
    There is also the option to automatically create a git repo and
    initialize it
-
-
-   TODO:
-      Check if project already exists in projects.txt
-      Add writing permissions to script
 """
 
 import os
-from sys import argv
+import stat
+from sys import argv, exit
 
 
 HOME = os.path.expanduser('~')
@@ -29,8 +25,8 @@ PROJECT_DIR = argv[1]
 def user_templates(templates):
     """ User selects template by choosing the number refering to that
         template. If template number doesn't exist, or if a non number is
-        selected then user is re-prompted.
-    """
+        selected then user is re-prompted. """
+
     no_of_templates = len(templates)
 
     try:
@@ -46,8 +42,8 @@ def user_templates(templates):
 
 
 def print_templates(templates):
-    """ Print available templates to terminal for user to choose from 
-    """
+    """ Print available templates to terminal for user to choose from. """
+
     print("\n{}====================".format(' ' * 5))
     print("{}CHOOSE YOUR TEMPLATE".format(' ' * 5))
     print("{}====================".format(' ' * 5))
@@ -60,8 +56,8 @@ def print_templates(templates):
 
 def get_templates():
     """ Return dictionary containing the available i3wm templates for user
-        to choose from.
-    """
+        to choose from. """
+
     templates = {}
     count = 1
     for f in os.listdir(TEMPLATES):
@@ -73,6 +69,10 @@ def get_templates():
 
 
 def create_script():
+    """ Open project template chosen by user and edit it to include data for
+        the new project. Then save this to a new file that is the bash script
+        for opening the new project. """
+
     templates = get_templates()
     print_templates(templates)
     chose = user_templates(templates)
@@ -81,11 +81,10 @@ def create_script():
 
     new_project_script = PROJECT_DIR.rstrip('/')
     new_project_script = os.path.split(new_project_script)[-1] + '.sh'
-    new_project_script = os.path.join(PROJECTS, new_project_script)
-    print(new_project_script)
+    new_project_script_absolute = os.path.join(PROJECTS, new_project_script)
 
     with open(script_template, 'r') as file1, \
-            open(new_project_script, 'w') as file2:
+            open(new_project_script_absolute, 'w') as file2:
 
         lines = file1.readlines()
         new_file = []
@@ -104,28 +103,52 @@ def create_script():
 
 
 def shorten_user():
-    """ Change '/home/user/' to '~' just to make it easier on the eye
-    """
+    """ Change '/home/user/' to '~' just to make it easier on the eye """
+
     return PROJECT_DIR.replace(HOME, "~")
 
 
 def new_index(projects):
     """ Establish how many projects already exist to give index number for new
-        project. If there are 5 projects, then this function returns '6'
-    """
+        project. If there are 5 projects, then this function returns '6' """
+
     length = len(projects)
     last_index = projects[length - 1].split(',')[0]
     return str(int(last_index) + 1)
 
 
-def create_project():
+def already_exist(projects):
+    """ Checks if project already exists """
 
- 
+    for project in projects:
+        if argv[1] in project or shorten_user() in project:
+            print("PROJECT LINE: {}".format(project))
+            return True
+            break
+
+
+def create_project():
+    """ Add the project data to file '~/i3-project-manager/project_list.txt'
+        and create the executable bash script that opens the project. """
+
     with open(PROJECTS_FILE, 'r+') as f:
         projects = f.readlines()
-        index = new_index(projects)
-        f.seek(0, 2)
-        f.write(index + ',' + shorten_user() + ',' + create_script())
+        if not already_exist(projects):
+            index = new_index(projects)
+            script = create_script()
+            script_path = os.path.join(PROJECTS, script)
+
+            # seek to end of file to append new project
+            f.seek(0, 2)
+            f.write(index + ',' + shorten_user() + ',' + script + "\n")
+
+            # make script executable for user
+            st = os.stat(script_path)
+            os.chmod(script_path, st.st_mode | stat.S_IEXEC)
+
+        else:
+            print("Project already exists\nAborting...")
+            exit(0)
 
 
 def main():
