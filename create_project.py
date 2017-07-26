c292326968f565a6510b4eb27dbe124896414462
Darren-Haynes <darrenhaynes@zoho.com>
2017-07-26 14:18:40 -0700

    added fucntion to automatically initiate git in project. needs work

--- create_project.py
+++ create_project.py
@@ -12,6 +12,7 @@
 
 import os
 import stat
+import subprocess
 from sys import argv, exit
 
 
@@ -151,31 +152,17 @@ def create_project():
             exit(0)
 
 
-def initialize_git():
-    """ Check if user wants to initialize a github repo for this project. """
-
-    answer = input("Do you want to initialize this project in git?\n").lower()
-
-    if answer != "yes" and answer != "y" and answer != "no" and answer != "n":
-        print("Please choose 'yes' or 'no' only'\n")
-        initialize_git()
-    elif answer == 'yes':
-        return True
-    elif answer == 'y':
-        return True
-    else:
-        print("Git not initialized\n") 
-        exit(0)
-
-
 def create_github_repo():
     if initialize_git():
         print("Now create git function")
+        os.chdir(PROJECT_DIR)
+        subprocess.call(["git", "init"])
+        subprocess.call(["touch", "README.md"])
+        subprocess.call(["cp", "/home/darren/.gitignore", PROJECT_DIR])
 
 
 def main():
     create_project()
-    create_github_repo()
 
 
 if __name__ == '__main__':
