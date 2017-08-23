b909e1b31c734492d9d7e87da67d93ceada6b833
Darren-Haynes <darrenhaynes@zoho.com>
2017-08-22 20:29:06 -0700

    removed selecting of colorschemes

--- projects/pyscripting.sh
+++ projects/pyscripting.sh
@@ -4,18 +4,14 @@ shopt -s globstar
 
 i3-msg "workspace Project; append_layout /home/darren/i3-project-manager/templates/pyscripting.json"
 
-python ~/Py_Scripts/xr_random_colors.py
 (urxvt -e vim ~/Py_Scripts/**/* &)
 sleep 0.1
 
-python ~/Py_Scripts/xr_random_colors.py
 (urxvt -e ipython3 &)
 sleep 0.01
 
-python ~/Py_Scripts/xr_random_colors.py
 (urxvt -e ipython2 &)
 sleep 0.01
 
-python ~/Py_Scripts/xr_random_colors.py
 (urxvt &)
 
