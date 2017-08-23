38ff040cbc06e16dd35eb0a220f8b7eb8ae3f049
Darren-Haynes <darrenhaynes@zoho.com>
2017-08-22 17:49:35 -0700

    changed to open files in gvim instead of vim

--- templates/codepen.sh
+++ templates/codepen.sh
@@ -7,23 +7,20 @@ cd $dir
 
 i3-msg "workspace Project; append_layout ^TEMP_DIR^"
 
+html_files="**/*.html"
+js_files="**/*.js"
+css_files="**/*.css"
 
-python ~/Py_Scripts/xr_random_colors.py
-(urxvt -e vim **/*.html &)
+
+(gvim $html_files)
 sleep 0.1
 
-
-python ~/Py_Scripts/xr_random_colors.py yes
-(urxvt -e vim **/*.js &)
+(gvim $js_files)
 sleep 0.1
 
-
-python ~/Py_Scripts/xr_random_colors.py
 (urxvt &)
 sleep 0.1
 
-
-python ~/Py_Scripts/xr_random_colors.py yes
-(urxvt -e vim **/*.css &)
+(gvim $css_files)
 sleep 0.1
 
